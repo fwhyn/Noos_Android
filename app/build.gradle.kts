a4e023bf7fc1d2c84db1c43f6590923330b58b5f
fwhyn <fwhynseventh@gmail.com>
2025-05-28 16:20:40 +0700

    Version Up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.fwhyn.app.noos"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1000000
-        versionName = "1.0.0"
+        versionCode = 2000000
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
